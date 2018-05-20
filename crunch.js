70c7bcc762646fd5a09738751685c7276f06d157
Ryan Dabler <rpdabler@gmail.com>
2018-05-19 19:45:29 -0600

    begin adding Crunch functionality

--- crunch.js
+++ crunch.js
@@ -8,3 +8,24 @@ const typeOf = obj =>
 
 const isIterable = obj =>
     obj ? typeOf(obj[Symbol.iterator]) === "Function" : false
+
+////////////////////
+// Main
+////////////////////
+class Crunch {
+    constructor(data) {
+        data.forEach((item, idx) => this[idx] = item);
+    }
+
+    [Symbol.iterator]() {
+        let i = 0;
+        return {
+            next() {
+                return this[i] ? { done: false, value: this[i] } : { done: true }
+            }
+        }
+    }
+}
+
+const crunch = data => 
+    isIterable(data) ? new Crunch(data) : new Crunch([])
