4da381547e9ad23eaec858c0739549e10a30a4b2
Ryan Dabler <rpdabler@gmail.com>
2018-05-19 21:03:38 -0600

    fix how object type is sliced out

--- crunch.js
+++ crunch.js
@@ -4,7 +4,7 @@
 const typeOf = obj =>
     Object.prototype.toString.call(obj)
         .split(" ")[1]
-        .slice(0, this.length - 1)
+        .slice(0, -1)
 
 const isIterable = obj =>
     obj ? typeOf(obj[Symbol.iterator]) === "Function" : false
