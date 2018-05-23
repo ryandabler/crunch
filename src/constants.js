7730f3b97a9b1d2e718a803a49e426cc0c1aba86
Ryan Dabler <rpdabler@gmail.com>
2018-05-23 16:41:12 -0600

    add typeOf constants

--- src/constants.js
+++ src/constants.js
@@ -1,4 +1,8 @@
 module.exports = Object.freeze({
+    TYPE_FUNCTION: "Function",
+    TYPE_OBJECT: "Object",
+    TYPE_NUMBER: "Number",
+    TYPE_ARRAY: "Array",
     MV_AVG_TRAIL: "TRAIL",
     MV_AVG_LEAD: "LEAD",
     MV_AVG_CENTER: "CENTER"
