3319e05a918612f99edd5f9e9bece41b5012f35c
Ryan Dabler <rpdabler@gmail.com>
2018-05-23 16:36:07 -0600

    move constants to new file

---
+++ src/constants.js
@@ -0,0 +1,5 @@
+module.exports = Object.freeze({
+    MV_AVG_TRAIL: "TRAIL",
+    MV_AVG_LEAD: "LEAD",
+    MV_AVG_CENTER: "CENTER"
+});
--- src/crunch.js
+++ src/crunch.js
@@ -1,9 +1,7 @@
 ////////////////////
-// Utilities
+// Initialize
 ////////////////////
-const MV_AVG_TRAIL = "TRAIL",
-      MV_AVG_LEAD = "LEAD",
-      MV_AVG_CENTER = "CENTER";
+const constants = require("./constants");
 
 ////////////////////
 // Utilities
