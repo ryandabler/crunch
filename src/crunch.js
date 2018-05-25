846c86e75b7e7124238457033a33e466100a03a6
Ryan Dabler <rpdabler@gmail.com>
2018-05-24 20:19:26 -0600

    JSDoc documentation for utility functions

--- src/crunch.js
+++ src/crunch.js
@@ -6,14 +6,36 @@ const constants = require("./constants");
 ////////////////////
 // Utilities
 ////////////////////
+
+/**
+ * Determines the type of any JavaScript element.
+ * 
+ * Takes the output from Object.prototype.toString() which is of the form
+ * "[object @@@@@]" and returns "@@@@@".
+ * 
+ * @param {*} obj Item to determine the type of
+ * @returns {string}
+ */
 const typeOf = obj =>
     Object.prototype.toString.call(obj)
         .split(" ")[1]
         .slice(0, -1)
 
+/**
+ * Determines whether supplied item is iterable.
+ * 
+ * @param {*} obj Item to determine iterability for
+ * @returns {boolean}
+ */
 const isIterable = obj =>
     obj ? typeOf(obj[Symbol.iterator]) === constants.TYPE_FUNCTION : false
 
+/**
+ * Ensures all elements of an iterable object are objects.
+ * 
+ * @param {Object} iterable Container whose elements should be converted into objects
+ * @returns {Array}
+ */
 const objectify = iterable => {
     const retObj = []
     for (let elem of iterable) {
@@ -25,6 +47,18 @@ const objectify = iterable => {
     return retObj;
 }
 
+/**
+ * Generates an object whose values come from the template with details supplied
+ * by groupBy.
+ * 
+ * Each condition in groupBy is converted into an object with the path specified by
+ * condition.name and the location of the value in template specified by condition.path.
+ * Each object is then merged into an accumulating object that is returned.
+ * 
+ * @param {Object} template Supplies the values for the final object
+ * @param {Array} groupBy Supplies the names and locations of the final object
+ * @returns {Object}
+ */
 const consolidateObj = (template, groupBy) => {
     let retObj = {}
 
@@ -40,6 +74,17 @@ const consolidateObj = (template, groupBy) => {
     return retObj;
 }
 
+/**
+ * Retrieves the value from a nested object given a path.
+ * 
+ * Iterates over an array containing the path to traverse in obj to find the value.
+ * The value of each step in the path should be an object except for the final stage.
+ * If any other step is not an object, should return null.
+ * 
+ * @param {Object} obj Object to retrieve value from
+ * @param {string} path Period-separated path to desired value
+ * @returns {*}
+ */
 const resolvePathAndGet = (obj, path) => {
     const segments = path.split(".");
     let pointer = obj;
@@ -53,6 +98,13 @@ const resolvePathAndGet = (obj, path) => {
     return pointer;
 }
 
+/**
+ * Creates a nested object whose keys are the specified path, terminating at the value.
+ * 
+ * @param {*} val Value to be inserted into object
+ * @param {string} path Specified path to the value
+ * @returns {Object}
+ */
 const resolvePathAndSet = (val, path) => {
     const retObj = {};
     let pointer = retObj;
@@ -65,6 +117,18 @@ const resolvePathAndSet = (val, path) => {
     return retObj;
 }
 
+/**
+ * Generates a calculation object for use in aggregating a collection of documents.
+ * 
+ * Splits a period-separated path string into segments and, for each segment,
+ * determines the operation and the parameter supplied to the operation. The
+ * parameter could be another calculation, so this object is filled out
+ * recursively.
+ * 
+ * @param {string} key Path from which to generate the operations
+ * @param {*} val Collection of paths to values in the object
+ * @param {number} call Recursive depth of the function
+ */
 // TODO: Rename these parameters to be more descriptive
 const generateCalculation = (key, val, call = 1) => {
     let name = "";
@@ -99,6 +163,18 @@ const generateCalculation = (key, val, call = 1) => {
     return param;
 }
 
+/**
+ * Merges an object of arbitrary depth into another object.
+ * 
+ * Traverses the main object, ensuring that the key in subObj is
+ * in mainObj, and recursively merging the next layer of each object.
+ * When a key is found in subObj that isn't in mainObj, the two 
+ * can be safely merged and the merged object is returned.
+ * 
+ * @param {Object} mainObj Object to have other object merged into
+ * @param {Object} subObj Object being merged
+ * @returns {Object}
+ */
 const mergeObjects = (mainObj, subObj) => {
     let retObj = { ...mainObj };
     for (const key in subObj) {
@@ -111,6 +187,16 @@ const mergeObjects = (mainObj, subObj) => {
     return retObj;
 }
 
+/**
+ * Sorts a nested object into grouping and calculation paths.
+ * 
+ * Destructures an object and then traverses each element to determine
+ * if it is calculation or grouping and adds it to the correct array.
+ * Returns an object containing both arrays.
+ * 
+ * @param {Object} obj
+ * @returns {Object}
+ */
 const siftObject = obj => {
     const flatObj = destructure(obj);
     const groupBy = [];
@@ -129,8 +215,26 @@ const siftObject = obj => {
     return { groupBy, calculations };
 }
 
+/**
+ * Creates a textual representation of the contents of an array.
+ * 
+ * @param {Array} arr 
+ * @returns {Array}
+ */
 const hashContents = arr => arr.map(elem => elem).join("")
 
+/**
+ * Traverses an object and flattens it.
+ * 
+ * Takes an object of arbitrary nestedness recursively traverses each path to
+ * generate a string representation of the path all the way down to the actual
+ * value. This path then gets added to a new object as key, with the indicated
+ * value as the value.
+ * 
+ * @param {Object} obj Object to flatten
+ * @param {string} [path=null] Path variable to track the recursive depth
+ * @returns {Object}
+ */
 const destructure = (obj, path = null) => {
     let retObj = {};
     const entries = Object.entries(obj);
@@ -148,6 +252,12 @@ const destructure = (obj, path = null) => {
 const aggregations = {
     functions: [ "$sum", "$avg" ],
 
+    /**
+     * Determines whether an object path represents a calculation directive.
+     * 
+     * @param {string} path 
+     * @returns {boolean}
+     */
     isFunctional(path) {
         return path.split(".")
             .reduce(
@@ -156,6 +266,19 @@ const aggregations = {
             );
     },
 
+    /**
+     * Sums up a series of documents.
+     * 
+     * Depending on the type of the param, different behavior is done.
+     * If param is a:
+     *     number, it merely gets added to reducedValue and returned.
+     *     array, the whole array is a list of paths to values that are added.
+     *     object, it is summing the output of another operation.
+     * 
+     * @param {Array} group 
+     * @param {*} param 
+     * @returns {number}
+     */
     $sum(group, param) {
         let reducedValue = 0;
         group.forEach(item => {
@@ -172,6 +295,19 @@ const aggregations = {
         return reducedValue;
     },
 
+    /**
+     * Averages a series of documents.
+     * 
+     * Depending on the type of the param, different behavior is done.
+     * If param is a:
+     *     number, it merely gets added to reducedValue and returned.
+     *     array, the whole array is a list of paths to values that are averaged.
+     *     object, it is averaging the output of another operation.
+     * 
+     * @param {Array} group 
+     * @param {*} param 
+     * @returns {number}
+     */
     $avg(group, param) {
         let reducedValue = 0;
         let counter = 0;
